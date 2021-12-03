727924d132f1854bfc84870fea71138bb480d9db
onib0002 <onib0002@algonquinlive.com>
2021-12-03 14:24:39 -0500

    created the tabnavigation

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './screen/Home';
+import List from './screen/List';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+    <NavigationContainer>
+      <Tab.Navigator
+            initialRouteName= "Home"
+            screenOption= {({route})=> ({
+              tabBarIcon: ({focused, color, size})=> {
+                let iconName;
+                if(route.name === 'Home') {
+                  iconName = focused ? 'ios-home' : 'ios-home-outline';
+                } else {
+                  iconName = focused ? 'list-circle' : 'list-circle-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+              }
+            })}
+      >
+        <Tab.Screen name="Home" component = {Home} />
+        <Tab.Screen name="List" component = {List} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screen/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Pressable,
+} from 'react-native';
+export default function List() {
+  return (
+    <Text>
+      List screen
+    </Text>
+  )
+}
